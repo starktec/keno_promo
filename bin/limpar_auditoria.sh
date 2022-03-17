b78a100017f85236fe838683f954ec4c41ad1bcf
David Fraga <david.j.fraga@gmail.com>
2022-03-17 18:30:43 -0300

    update conf server

--- bin/limpar_auditoria.sh
+++ bin/limpar_auditoria.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /home/project/bingo/env/bin/activate
-python /home/project/bingo/manage.py runscript limpar_auditoria
+source /home/project/keno_promo/env/bin/activate
+python /home/project/keno_promo/manage.py runscript limpar_auditoria
 sync && sudo sysctl -w vm.drop_caches=3
 systemctl restart redis
 systemctl restart postgresql
