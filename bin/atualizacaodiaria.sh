6900e866df092a66550b48e1e4303d6a5805f1cf
David Fraga <david.j.fraga@gmail.com>
2022-06-08 18:21:28 -0300

    cron para atualizacao diaria

---
+++ bin/atualizacaodiaria.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /home/project/keno_promo/env/bin/activate
+python /home/project/keno_promo/manage.py runscript limpar_auditoria
+sync && sudo sysctl -w vm.drop_caches=3
+systemctl restart redis
+systemctl restart postgresql
+systemctl restart gunicorn
+
+  
