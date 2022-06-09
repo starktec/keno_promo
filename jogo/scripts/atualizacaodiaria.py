603208ad750d7edd1f1daf01cc68e8609aab994c
David Fraga <david.j.fraga@gmail.com>
2022-06-09 18:08:13 -0300

    relacionando script com contas do back, pegar bilhete

--- jogo/scripts/atualizacaodiaria.py
+++ jogo/scripts/atualizacaodiaria.py
@@ -75,166 +75,168 @@ def run():
 
     clients = []
     posicao = 0
-    save = True
+    save = False
     try:
         CONTAS = Conta.objects.filter(ativo=True, atencao=False)
-        proxies = IPTabela.objects.last()
-        proxy_list = []
-        if proxies:
-            proxy_list = [p.ip_proxy+":"+p.ip_faixa for p in proxies]
-        contador = 0
-        for i_conta in CONTAS:
-            log(f"Fazendo login na conta {i_conta.get('username')}...")
-            time.sleep(15)
-            con_client = Client()
-            if proxy_list:
-                con_client.set_proxy(proxy_list[contador])
-            con_client.login(i_conta.username, i_conta.password)
-            clients.append((con_client,i_conta.username))
-            i_conta.instagram_connection = pickle.dumps(con_client)
-            i_conta.ultimo_acesso = datetime.now()
-            i_conta.save()
+        if CONTAS:
+            proxies = IPTabela.objects.last()
+            proxy_list = []
+            if proxies:
+                proxy_list = [p.ip_proxy+":"+p.ip_faixa for p in proxies]
+            contador = 0
+            for i_conta in CONTAS:
+                log(f"Fazendo login na conta {i_conta.get('username')}...")
+                time.sleep(15)
+                con_client = Client()
+                if proxy_list:
+                    con_client.set_proxy(proxy_list[contador])
+                con_client.login(i_conta.username, i_conta.password)
+                clients.append((con_client,i_conta.username))
+                i_conta.instagram_connection = pickle.dumps(con_client)
+                i_conta.ultimo_acesso = datetime.now()
+                i_conta.save()
 
-            contador += 1
+                contador += 1
 
-        client, connection = clients[posicao%len(CONTAS)]
-        log(f"({connection}) Buscando seguidores")
-        posicao += 1
-        antes = datetime.now()
-        seguidores = client.user_followers(JOGOSDASORTEBR_ID)
-        agora = datetime.now()
-        log(f" - {len(seguidores)} Seguidores encontrados em {int((agora-antes).seconds/60)} minutos")
+            client, connection = clients[posicao%len(CONTAS)]
+            log(f"({connection}) Buscando seguidores")
+            posicao += 1
+            antes = datetime.now()
+            seguidores = client.user_followers(JOGOSDASORTEBR_ID)
+            agora = datetime.now()
+            log(f" - {len(seguidores)} Seguidores encontrados em {int((agora-antes).seconds/60)} minutos")
 
-        log(" - Registrando csv...")
-        hoje = date.today()
-        nome_arquivo = f"{hoje.year}{hoje.month}{hoje.day}_seguidores.csv"
-        arquivo = open(settings.BASE_DIR + "/logs/"+nome_arquivo,"w",encoding="utf-8")
+            log(" - Registrando csv...")
+            hoje = date.today()
+            nome_arquivo = f"{hoje.year}{hoje.month}{hoje.day}_seguidores.csv"
+            arquivo = open(settings.BASE_DIR + "/logs/"+nome_arquivo,"w",encoding="utf-8")
 
-        lista_seguidores = []
+            lista_seguidores = []
 
-        for seguidor in seguidores.values():
-            pic_url = ""
-            if seguidor.profile_pic_url:
-                pic_url = str(seguidor.profile_pic_url)
-            lista = (seguidor.pk,seguidor.username,seguidor.full_name,pic_url)
-            lista_seguidores.append(Seguidor(seguidor.pk,seguidor.username,seguidor.full_name,pic_url))
-            arquivo.write("|".join(lista)+"\n")
-        arquivo.close()
+            for seguidor in seguidores.values():
+                pic_url = ""
+                if seguidor.profile_pic_url:
+                    pic_url = str(seguidor.profile_pic_url)
+                lista = (seguidor.pk,seguidor.username,seguidor.full_name,pic_url)
+                lista_seguidores.append(Seguidor(seguidor.pk,seguidor.username,seguidor.full_name,pic_url))
+                arquivo.write("|".join(lista)+"\n")
+            arquivo.close()
 
-        log(" - Processado. Entrando na Fase 2: atualizando o banco de dados")
-        contas = InstagramAccount.objects.all()
-        jogadores = Jogador.objects.all()
+            log(" - Processado. Entrando na Fase 2: atualizando o banco de dados")
+            contas = InstagramAccount.objects.all()
+            jogadores = Jogador.objects.all()
 
-        seguidores_por_id = {x.pk: x for x in seguidores}
-        seguidores_por_username = {x.username: x for x in seguidores}
-        update_jogadores = []
-        fake = []
-        nao_segue = []
-        for jogador in jogadores:
-            log(f"START {jogador.usuario}")
-            seguidor = None
-            if jogador.usuario_id in seguidores_por_id.keys():
-                seguidor = seguidores_por_id[jogador.usuario_id]
-            else:
-                if jogador.usuario in seguidores_por_username.keys():
-                    seguidor = seguidores_por_username[jogador.usuario]
-            if seguidor:
-                log(f"{jogador.usuario} veio na lista de seguidores")
-                conta = contas.filter(instagram_id=seguidor.pk).first()
-                if conta:
-                    log(f"{jogador.usuario} possui um registro local")
-                    if conta.recente:
-                        log(f"{jogador.usuario} deixa de ser um registro recente")
-                        conta.recente = False
-                        conta.save()
+            seguidores_por_id = {x.pk: x for x in seguidores}
+            seguidores_por_username = {x.username: x for x in seguidores}
+            update_jogadores = []
+            fake = []
+            nao_segue = []
+            for jogador in jogadores:
+                log(f"START {jogador.usuario}")
+                seguidor = None
+                if jogador.usuario_id in seguidores_por_id.keys():
+                    seguidor = seguidores_por_id[jogador.usuario_id]
                 else:
-                    log(f"{jogador.usuario} NÃO possui um registro local... criando um novo")
-                    conta = InstagramAccount.objects.create(
-                        instagram_id=seguidor.pk,username=seguidor.username,full_name=seguidor.full_name,
-                        pic=seguidor.pic
-                    )
-                log(f"{jogador.usuario} sendo atualizado e forçando seu status para ATIVO")
-                j_update = update_jogador(jogador,conta,StatusJogador.ATIVO)
-                if j_update:
-                    num_jogadores_ativo_novos += 1
-                    j_update.save()
-            else:
-                log(f"{jogador.usuario} NÃO veio na lista de seguidores")
-                conta = InstagramAccount.objects.none()
-                if jogador.usuario_id:
-                    conta = contas.filter(instagram_id=jogador.usuario_id).first()
-                else:
-                    conta = contas.filter(username=jogador.usuario).first()
-
-                if conta:
-                    log(f"{jogador.usuario} possui um registro local")
-                    j_update = None
-                    if not conta.recente:
-                        log(f"{jogador.usuario} suspendendo conta antiga (não participa de jogos)")
-                        j_update = update_jogador(jogador, conta, StatusJogador.SUSPENSO)
-                        num_jogadores_suspenso_novos += 1
-                        nao_segue.append(jogador.usuario)
+                    if jogador.usuario in seguidores_por_username.keys():
+                        seguidor = seguidores_por_username[jogador.usuario]
+                if seguidor:
+                    log(f"{jogador.usuario} veio na lista de seguidores")
+                    conta = contas.filter(instagram_id=seguidor.pk).first()
+                    if conta:
+                        log(f"{jogador.usuario} possui um registro local")
+                        if conta.recente:
+                            log(f"{jogador.usuario} deixa de ser um registro recente")
+                            conta.recente = False
+                            conta.save()
                     else:
-                        tempo_em_dias = (agora - conta.data_cadastro).days
-                        if tempo_em_dias>1:
-                            log(f"{jogador.usuario} suspendendo conta antiga (acima de 1 dia não participa de jogos)")
+                        log(f"{jogador.usuario} NÃO possui um registro local... criando um novo")
+                        conta = InstagramAccount.objects.create(
+                            instagram_id=seguidor.pk,username=seguidor.username,full_name=seguidor.full_name,
+                            pic=seguidor.pic
+                        )
+                    log(f"{jogador.usuario} sendo atualizado e forçando seu status para ATIVO")
+                    j_update = update_jogador(jogador,conta,StatusJogador.ATIVO)
+                    if j_update:
+                        num_jogadores_ativo_novos += 1
+                        j_update.save()
+                else:
+                    log(f"{jogador.usuario} NÃO veio na lista de seguidores")
+                    conta = InstagramAccount.objects.none()
+                    if jogador.usuario_id:
+                        conta = contas.filter(instagram_id=jogador.usuario_id).first()
+                    else:
+                        conta = contas.filter(username=jogador.usuario).first()
+
+                    if conta:
+                        log(f"{jogador.usuario} possui um registro local")
+                        j_update = None
+                        if not conta.recente:
+                            log(f"{jogador.usuario} suspendendo conta antiga (não participa de jogos)")
                             j_update = update_jogador(jogador, conta, StatusJogador.SUSPENSO)
                             num_jogadores_suspenso_novos += 1
                             nao_segue.append(jogador.usuario)
                         else:
-                            log(f"{jogador.usuario} é uma conta recente... aguardando mais 1 dia para vir na lista diária")
+                            tempo_em_dias = (agora - conta.data_cadastro).days
+                            if tempo_em_dias>1:
+                                log(f"{jogador.usuario} suspendendo conta antiga (acima de 1 dia não participa de jogos)")
+                                j_update = update_jogador(jogador, conta, StatusJogador.SUSPENSO)
+                                num_jogadores_suspenso_novos += 1
+                                nao_segue.append(jogador.usuario)
+                            else:
+                                log(f"{jogador.usuario} é uma conta recente... aguardando mais 1 dia para vir na lista diária")
 
-                    if j_update:
-                        j_update.save()
-                else:
-                    log(f"{jogador.usuario} Nem tem um registro local nem veio na lista de seguidores...")
-                    time.sleep(int(TIME_MIN/len(CONTAS))+1)
-                    client, connection = clients[posicao % len(CONTAS)]
-                    log(f"({connection}) Buscando dados do perfil @{jogador.usuario} no instagram")
-                    posicao += 1
-                    try:
-                        usuario = client.user_info_by_username(jogador.usuario)
-                        if not usuario:
-                            raise ClientNotFoundError()
-                    except (ClientNotFoundError, UserNotFound) as cnfe:
-                        log(f"@{jogador.usuario} é uma conta inexistente... cancelando jogador")
-                        # Conta nao existe
-                        jogador.status = StatusJogador.CANCELADO
-                        num_jogadores_cancelado_novos += 1
-                        jogador.save()
-                        fake.append(jogador.usuario)
-                        continue
-
-                    time.sleep(int(TIME_MIN / len(CONTAS)) + 1)
-                    client, connection = clients[posicao % len(CONTAS)]
-                    log(f"({connection}) Confirmando se @{jogador.usuario} segue o {PERFIL} no instagram")
-                    posicao += 1
-                    usuarios = client.search_followers(user_id=JOGOSDASORTEBR_ID,query=jogador.usuario)
-                    #seguidor_instagram = [x for x in seguidores_instagram if x.username == jogador.usuario]
-
-                    usuarios_dict = {u.username: u for u in usuarios}
-                    if usuarios and jogador.usuario in usuarios_dict.keys():
-                        log(f"@{jogador.usuario} encontrado. Criando registro e atualizado jogador...")
-                        # Passou no teste atualizando
-                        usuario = usuarios_dict.get(jogador.usuario)
-                        conta = InstagramAccount.objects.create(
-                            instagram_id=usuario.pk,
-                            username=usuario.username,
-                            full_name=usuario.full_name,
-                            pic=str(usuario.profile_pic_url)
-                        )
-                        jogador.conta = conta
-                        jogador.usuario_id = conta.instagram_id
-                        jogador.status = StatusJogador.ATIVO
-                        num_jogadores_ativo_novos += 1
-                        jogador.foto = download_foto(conta.pic, conta.instagram_id)
+                        if j_update:
+                            j_update.save()
                     else:
-                        # Usuario nao segue a Jogosdasortebr
-                        jogador.status = StatusJogador.SUSPENSO
-                        num_jogadores_suspenso_novos += 1
-                        log(f"@{jogador.usuario} Usuario existe mas nao segue o {PERFIL}")
-                        nao_segue.append(jogador.usuario)
-                    jogador.save()
+                        log(f"{jogador.usuario} Nem tem um registro local nem veio na lista de seguidores...")
+                        time.sleep(int(TIME_MIN/len(CONTAS))+1)
+                        client, connection = clients[posicao % len(CONTAS)]
+                        log(f"({connection}) Buscando dados do perfil @{jogador.usuario} no instagram")
+                        posicao += 1
+                        try:
+                            usuario = client.user_info_by_username(jogador.usuario)
+                            if not usuario:
+                                raise ClientNotFoundError()
+                        except (ClientNotFoundError, UserNotFound) as cnfe:
+                            log(f"@{jogador.usuario} é uma conta inexistente... cancelando jogador")
+                            # Conta nao existe
+                            jogador.status = StatusJogador.CANCELADO
+                            num_jogadores_cancelado_novos += 1
+                            jogador.save()
+                            fake.append(jogador.usuario)
+                            continue
+
+                        time.sleep(int(TIME_MIN / len(CONTAS)) + 1)
+                        client, connection = clients[posicao % len(CONTAS)]
+                        log(f"({connection}) Confirmando se @{jogador.usuario} segue o {PERFIL} no instagram")
+                        posicao += 1
+                        usuarios = client.search_followers(user_id=JOGOSDASORTEBR_ID,query=jogador.usuario)
+                        #seguidor_instagram = [x for x in seguidores_instagram if x.username == jogador.usuario]
+
+                        usuarios_dict = {u.username: u for u in usuarios}
+                        if usuarios and jogador.usuario in usuarios_dict.keys():
+                            log(f"@{jogador.usuario} encontrado. Criando registro e atualizado jogador...")
+                            # Passou no teste atualizando
+                            usuario = usuarios_dict.get(jogador.usuario)
+                            conta = InstagramAccount.objects.create(
+                                instagram_id=usuario.pk,
+                                username=usuario.username,
+                                full_name=usuario.full_name,
+                                pic=str(usuario.profile_pic_url)
+                            )
+                            jogador.conta = conta
+                            jogador.usuario_id = conta.instagram_id
+                            jogador.status = StatusJogador.ATIVO
+                            num_jogadores_ativo_novos += 1
+                            jogador.foto = download_foto(conta.pic, conta.instagram_id)
+                        else:
+                            # Usuario nao segue a Jogosdasortebr
+                            jogador.status = StatusJogador.SUSPENSO
+                            num_jogadores_suspenso_novos += 1
+                            log(f"@{jogador.usuario} Usuario existe mas nao segue o {PERFIL}")
+                            nao_segue.append(jogador.usuario)
+                        jogador.save()
+            save = True
     except Exception as e:
         log(e.__dict__)
         save = False
