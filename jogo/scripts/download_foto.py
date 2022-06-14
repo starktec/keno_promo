9b291092fa857fc0989a91774e4a647109e073a5
David Fraga <david.j.fraga@gmail.com>
2022-06-14 11:38:36 -0300

    fix download foto

--- jogo/scripts/download_foto.py
+++ jogo/scripts/download_foto.py
@@ -4,5 +4,7 @@ def run():
     for jogador in Jogador.objects.filter(conta__isnull=False, foto__isnull=True):
         print(f"verficando @{jogador.usuario}")
         conta = jogador.conta
+        print(f" - conta {conta.id}")
         jogador.foto = download_foto(conta.pic, conta.instagram_id)
+        print(f" - foto {jogador.foto}")
         jogador.save()
