63cda805f2b685c0a7902a3d3c0dad77c6c476d7
david <david.fraga@gds.games>
2022-12-16 17:16:47 -0300

    flix

--- jogo/consumers.py
+++ jogo/consumers.py
@@ -11,6 +11,105 @@ logger = logging.getLogger(__name__)
 
 GROUPS = []
 
+
+class ConexaoTelaConsumer(AsyncJsonWebsocketConsumer):
+    @sync_to_async
+    def proximos_sorteios(self):
+        proximos = Partida.objects.filter(
+            data_partida__gt=datetime.datetime.now(), em_sorteio=False, bolas_sorteadas__isnull=True, tipo_rodada=1
+        ).order_by('data_partida')[:5]
+        serializer = PartidaProximaSerializer(instance=proximos, many=True)
+        logger.info(f" - Próximos: {[str(proximo.id) for proximo in proximos]}")
+        return {"type": "proximos_sorteios", "sorteios": serializer.data, 'datahora': datetime.datetime.strftime(
+            datetime.datetime.now(), '%Y-%m-%dT%H:%M:%S')}
+
+    @sync_to_async
+    def proximos_especiais(self):
+        proximos = Partida.objects.filter(
+            data_partida__gt=datetime.datetime.now(), em_sorteio=False, bolas_sorteadas__isnull=True,
+            tipo_rodada=2
+        ).order_by('data_partida')[:5]
+        serializer = PartidaProximaEspecialSerializer(instance=proximos, many=True)
+        logger.info(f" - Próximos: {[str(proximo.id) for proximo in proximos]}")
+        return {"type": "proximos_especiais", "sorteios": serializer.data, 'datahora': str(datetime.datetime.now())}
+
+    @sync_to_async
+    def proximos_s_especiais(self):
+
+        proximos = Partida.objects.filter(
+            data_partida__gt=datetime.datetime.now(), em_sorteio=False, bolas_sorteadas__isnull=True,
+            tipo_rodada=3
+        ).order_by('data_partida')[:5]
+        serializer = PartidaProximaEspecialSerializer(instance=proximos, many=True)
+        logger.info(f" - Próximos: {[str(proximo.id) for proximo in proximos]}")
+        return {"type": "proximos_s_especiais", "sorteios": serializer.data, 'datahora': str(datetime.datetime.now())}
+
+    @sync_to_async
+    def ultimos_ganhadores(self):
+        vencedores = []
+        configuracao = Configuracao.objects.last()
+        data_liberacao = datetime.datetime.now() - datetime.timedelta(
+            minutes=configuracao.liberar_resultado_sorteio_em)
+        for v in CartelaVencedora.objects.filter(
+                partida__data_partida__lte=data_liberacao
+        ).order_by('-id')[:6]:
+            d = {
+                'sorteio': int(v.partida.id),
+                'cartela': int(v.cartela.codigo),
+                'nome': str(v.cartela.pdv.estabelecimento),
+                'premio': float(v.valor_premio),
+                'tipo': int(v.premio) if not v.ganhou_acumulado else 4
+            }
+            vencedores.append(d)
+        return {'type': 'vencedores', 'vencedores': vencedores}
+
+
+    async def connect(self):
+        logger.info(f"WS Connect {self.channel_name} - TOKEN {self.scope['url_route']['kwargs']['token']}")
+        self.user = self.scope["user"]
+        self.token = self.scope['url_route']['kwargs']['token']
+        self.nome_sala = 'tela-connect'
+        if self.nome_sala not in GROUPS:
+            GROUPS.append(self.nome_sala)
+
+        await self.accept()
+        await self.channel_layer.group_add(  # criando um layer group
+            self.nome_sala,
+            self.channel_name
+        )
+
+        await self.send_json({'code': 200, 'message': 'Conexao estabelecida!', 'error_message': '', 'data': {}})
+        proximos_sorteios = await self.proximos_sorteios()
+        proximos_especiais = await self.proximos_especiais()
+        proximos_se_especiais = await self.proximos_s_especiais()
+        ultimos_ganhadores = await self.ultimos_ganhadores()
+        await self.send_json(proximos_sorteios)
+        await self.send_json(proximos_especiais)
+        await self.send_json(proximos_se_especiais)
+        await self.send_json(ultimos_ganhadores)
+
+    async def disconnect(self, code):
+        logger.info(f"WS Disconnect {self.channel_name}")
+        await self.desconectar_tela()
+        return await super().disconnect(code)
+
+    async def events_tela_partidas(self, event):
+        try:
+            proximos_sorteios = await self.proximos_sorteios()
+            proximos_especiais = await self.proximos_especiais()
+            proximos_s_especiais = await self.proximos_s_especiais()
+            await self.send_json(proximos_sorteios)
+            await self.send_json(proximos_especiais)
+            await self.send_json(proximos_s_especiais)
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+    async def events_tela_sorteio(self, event):
+        sorteio = event.get('id_sorteio')
+        logger.info(f"WS Notificando sorteio {sorteio}")
+        await self.send_json({"type": "sorteio", "id_sorteio": sorteio})
+
 class DadosTempoRealConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         try:
--- jogo/routing.py
+++ jogo/routing.py
@@ -6,4 +6,5 @@ from . import consumers
 websocket_urlpatterns = [
   path('ws/<str:token>/sorteio/<int:sorteio>/bilhete/<str:bilhete>/connect/',consumers_bilhete.ConexaoBilheteConsumer.as_asgi()),
   path('ws/dados/realtime/', consumers.DadosTempoRealConsumer.as_asgi()),
+  path('ws/tela/<str:token>/connect/',consumers.ConexaoTelaConsumer.as_asgi()),
 ]
