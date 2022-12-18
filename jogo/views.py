4c29375d4c52f4e705c2576ee6dd4149a8e9ff76
david <david.fraga@gds.games>
2022-12-18 00:49:37 -0300

    fix

--- jogo/views.py
+++ jogo/views.py
@@ -964,6 +964,8 @@ class PegarCartela(APIView):
                         mensagem = "Cartelas esgotadas"
                         LOGGER.info(mensagem)
                         return Response(data={"detail": mensagem}, status=404)
+            else:
+                cartela = cartela.first()
             return Response(
                 data={"cartela":[int(c.id) for c in cartela], "bilhete": cartela.hash, "sorteio": int(cartela.first().partida.id)})
 
